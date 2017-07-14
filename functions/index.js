702233ae8e6fc2b4165828b44562df9a5ff6489f
wewillfindawaywealwayshave <openproject@Juneui-MacBook-Pro.local>
2017-07-15 06:59:42 +0900

    펑션/리얼디비 가이드 추가

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+//디비에 읽는거
+exports.practice_write_function = functions.https.onRequest((request, response) => {
+  console.log('start practice_write_function');
+ const test_ref = admin.database().ref('/test');
+
+ test_ref.child("여기에 이름을 적으세욤").set("success")
+
+ response.send("wrtie finish")
 });
 
-exports.detail_page = functions.https.onRequest((request, response) => {
- response.redirect('https://opensourceproject-73d9a.firebaseapp.com/detail_page.html');
+//디비쓰는거
+exports.practice_read_function = functions.https.onRequest((request, response) => {
+console.log('start practice_read_function');
+ const test_ref = admin.database().ref('/test');
+
+ test_ref.once("value", function(data){
+   response.send("내 디비안에 있는 내용들:"+JSON.stringify(data.val()))
+  });
 });
 
-exports.rules_page = functions.https.onRequest((request, response) => {
- response.redirect('https://opensourceproject-73d9a.firebaseapp.com/usage-rules.html');
+//페이지 이동시킬때ㅇㅇ
+exports.practice_move_function = functions.https.onRequest((request, response) => {
+console.log('start practice_move_function');
+ const test_ref = admin.database().ref('/test');
+
+ //이런식으로 이동ㅇㅇ
+ response.redirect('https://us-central1-opensourceproject-73d9a.cloudfunctions.net/detail_page');
 });
